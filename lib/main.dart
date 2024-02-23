164c5543338c9aca117083b8c95094dd10a1e537
Shamsiddin88 <145777247+Shamsiddin88@users.noreply.github.com>
2024-02-23 20:31:31 +0500

    Revert "Shared preferences saving"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:home_tasks/data/local/storage_repository.dart';
-import 'package:home_tasks/screens/auth_screen/auth_screen.dart';
-import 'package:home_tasks/screens/boarding_screen/boarding_screen.dart';
-import 'package:home_tasks/screens/home_screen.dart';
-import 'package:home_tasks/screens/splash_screen/splash_screen.dart';
+import 'package:home_tasks/screens/HomeScreen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
+void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: false),
-      home: SplashScreen(),
+      home: HomeScreen(),
     ),
   );
 }
