fb84e3fc11ab746aaedeecc1ed3ba1139c34d22a
Shamsiddin Djuraev <shams_@bk.ru>
2024-02-23 20:45:02 +0500

    bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:home_tasks/data/local/storage_repository.dart';
-import 'package:home_tasks/screens/auth_screen/auth_screen.dart';
-import 'package:home_tasks/screens/boarding_screen/boarding_screen.dart';
 import 'package:home_tasks/screens/home_screen.dart';
-import 'package:home_tasks/screens/splash_screen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +8,7 @@ Future<void> main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: false),
-      home: SplashScreen(),
+      home: HomeScreen(),
     ),
   );
 }
